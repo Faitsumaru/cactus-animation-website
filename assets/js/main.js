a8c359533d06425d3e43b4e0c43e5ce8bb9f0ad5
Faitsumaru <kiryakov.g.m@gmail.com>
2024-03-16 20:13:40 +0300

    header & nav logic (show & hide menu)

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,22 @@
 /*=============== SHOW MENU ===============*/
+const navMenu = document.getElementById('nav-menu'),
+    navToggle = document.getElementById('nav-toggle'),
+    navClose = document.getElementById('nav-close')
 
+// show menu
+if (navToggle) {
+    navToggle.addEventListener('click', () => {
+        navMenu.classList.add('show-menu')
+    })
+}
 
-/*=============== REMOVE MENU MOBILE ===============*/
+// hide menu
+if (navClose) {
+    navClose.addEventListener('click', () => {
+        navMenu.classList.remove('show-menu')
+    })
+}
+
 
 
 /*=============== ADD BLUR HEADER ===============*/
